bba3515a8dbaaeebcc0a5bfa1ec7845571daaa85
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2022-07-03 02:57:18 +0430

    modify the node to do some operations

--- fibonacci_heap/Node.py
+++ fibonacci_heap/Node.py
@@ -1,10 +1,33 @@
 class Node(object):
-    def __init__(self, key, value):
+    def __init__(self, key, value=None):
         self.key = key
         self.value = value
-        self.parent = self.child = self.right = self.left = None
+        self.child = self.right = self.left = None
+        self.parents = None
         self.degree = 0
         self.marked = False
+
+    def add_node(self, node):
+        if self.child is None:
+            self.child = [node]
+            self._set_parent(node)
+            self.degree += 1
+
+        else:
+            self.child.append(node)
+            self._set_parent(node)
+            self.degree += 1
+
+    def delete_node(self, node):
+        self.child.remove(node)
+        self.degree -= 1
+        node.parent = None
+
+    def _set_parent(self, node):
+        if node.parents is None:
+            node.parents = [self]
+        else:
+            node.parents.append(self)
 
     def get_value(self):
         return self.value
